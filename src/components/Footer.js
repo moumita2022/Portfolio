08b10c1ef04d5a0b28622eb32109717d9bb75baa
moumita2022 <moumitachakr@googlemail.com>
2023-02-09 16:21:47 +0100

    code correction

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,6 @@ import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-
 export const Footer = () => {
   return (
     <footer className="footer">
@@ -18,18 +15,59 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a
-                href="https://www.linkedin.com/in/moumita-chakraborty-das-33b035231/"
-                target="_blank"
-                rel="noopener noreferrer"
-              ></a>
+              <a href="#"><img src={navIcon1} alt="Icon" /></a>
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#https://github.com/moumita2022" target ="blank" rel="nooperner noreferrer"><GitHubIcon className="icons" fontSize="large" /></a>
+              <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright&copy; 2023. All Rights Reserved</p>
+            <p>Copyright 2022. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
     </footer>
   )
 }
+// import { Container, Row, Col } from "react-bootstrap";
+// import { MailchimpForm } from "./MailchimpForm";
+// import LinkedInIcon from "@mui/icons-material/LinkedIn";
+// import GitHubIcon from "@mui/icons-material/GitHub";
+// export const Footer = () => {
+//   return (
+//     <footer className="footer">
+//       <Container>
+//         <Row className="align-items-center">
+//           <MailchimpForm />
+//           <Col size={12} sm={6}>
+//           <Link href="/">
+//               <a className="d-flex align-items-center navbar-brand">
+//                 <Image
+//                   src="/img/logo.svg"
+//                   alt="logo"
+//                   width={150}
+//                   height={150}
+//                 />
+//                 <div>
+//                   <p className="m-0 fs-5">Hammed Waheed</p>
+//                   <p className="m-0 fs-6">Fullstack Web Developer</p>
+//                 </div>
+//               </a>
+//             </Link>
+//           </Col>
+//           <Col size={12} sm={6} className="text-center text-sm-end">
+//             <div className="social-icon">
+//               <a
+//                 href="https://www.linkedin.com/in/waheed-hammed/"
+//                 target="_blank"
+//                 rel="noopener noreferrer"
+//               >
+//                 <LinkedInIcon className="icons" fontSize="large" />
+//               </a>
+//               <a href="https://github.com/Hammed-Waheed" target="_blank"
+//                 rel="noopener noreferrer"><GitHubIcon className="icons" fontSize="large" /></a>
+//             </div>
+//             <p>Copyright&copy; 2023. All Rights Reserved</p>
+//           </Col>
+//         </Row>
+//       </Container>
+//     </footer>
+//   )
+// }
