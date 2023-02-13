2937864f80b72c9afec3bc347b138c7394b97b3b
moumita2022 <moumitachakr@googlemail.com>
2023-02-13 13:51:02 +0100

    topic

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
                 >
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>
-                    {`Hi I'm Hammed `}
+                    {`Hi I'm Moumita`}
                     <span
                       className="txt-rotate"
                       dataPeriod="1000"
