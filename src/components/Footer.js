db3df71864ae9a059f286baf186de2fef84e5efa
moumita2022 <moumitachakr@googlemail.com>
2023-02-14 13:27:08 +0100

    Worked on Navbar and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,18 +11,22 @@ export const Footer = () => {
         <Row className="align-items-center">
           <MailchimpForm />
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+
+            <h1 className="logos">MOMO</h1>
+            {/* <img src={logo} alt="Logo" /> */}
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              
+              <a href="https://github.com/moumita2022" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/moumita-chakraborty-das-33b035231/" target="_blank"><img src={navIcon1} alt="Icon" /></a>
+              {/* <a href="#"><img src={navIcon3} alt="Icon" /></a> */}
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
+  
     </footer>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -32,7 +32,8 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
+            <h1 className="logos">MOMO</h1>
+            {/* <img src={logo} alt="Logo" /> */}
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
